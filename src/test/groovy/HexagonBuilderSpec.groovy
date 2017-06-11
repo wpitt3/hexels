7bed1d23895f439964d16e5888b5505a6194f972
Will Pitt <wpitt3@gmail.com>
2017-06-11 21:19:54 +0100

    tidy

--- src/test/groovy/HexagonBuilderSpec.groovy
+++ src/test/groovy/HexagonBuilderSpec.groovy
@@ -3,13 +3,12 @@ import spock.lang.Specification
 /**
  * Created by willpitt on 07/06/2017.
  */
-class HexelsTest extends Specification{
-    Double size, xPos, yPos, delta, radius
+class HexagonBuilderSpec extends Specification{
+    Double xPos, yPos, delta, radius
     Integer edges, width, height
-    HexelBuilder hexelBuilder
+    HexagonBuilder hexagonBuilder
 
     void setup() {
-        size = 20
         xPos = 100
         yPos = 100
         delta = 0.001
@@ -17,35 +16,35 @@ class HexelsTest extends Specification{
         width = 1000
         height = 1000
         radius = 100
-        hexelBuilder = new HexelBuilder([width:width, height:height, radius:radius])
+        hexagonBuilder = new HexagonBuilder(width, height, radius)
     }
 
     void "all vertices are 'size' distance from centre"() {
         when:
-        List<List> hexPoints = hexelBuilder.createShape(size, xPos, yPos, edges)
+        List<List> hexPoints = hexagonBuilder.createShape(xPos, yPos, edges)
         then:
         hexPoints.every { point ->
             Double xDiff = (point[0]-xPos)**2
             Double yDiff = (point[1]-yPos)**2
-            (xDiff + yDiff) - size**2 < delta
+            (xDiff + yDiff) - radius**2 < delta
         }
     }
 
     void "all vertices are 'size' distance from next point"() {
         when:
-        List<List> hexPoints = hexelBuilder.createShape(size, xPos, yPos, edges)
+        List<List> hexPoints = hexagonBuilder.createShape(xPos, yPos, edges)
         then:
         (0..<hexPoints.size()).every { index ->
             List nextPoint = hexPoints[(index+1)%hexPoints.size()]
             Double xDiff = (hexPoints[index][0]-nextPoint[0])**2
             Double yDiff = (hexPoints[index][1]-nextPoint[1])**2
-            (xDiff + yDiff) - size**2 < delta
+            (xDiff + yDiff) - radius**2 < delta
         }
     }
 
     void "hexagon has horizontal lines on top bottom and middle"() {
         when:
-        List<List> hexPoints = hexelBuilder.createShape(size, xPos, yPos, edges)
+        List<List> hexPoints = hexagonBuilder.createShape(xPos, yPos, edges)
         then:
         hexPoints[0][1] - hexPoints[3][1] < delta // across middle
         hexPoints[1][1] - hexPoints[2][1] < delta // bottom
@@ -54,34 +53,34 @@ class HexelsTest extends Specification{
 
     void "calculate number of columns fits exactly"() {
         when:
-        hexelBuilder.radius = 101
-        hexelBuilder.width = 961
-        Integer numberOfColumns = hexelBuilder.calculateNoOfColumns(hexelBuilder.calculateXOffset())
+        hexagonBuilder.radius = 101
+        hexagonBuilder.width = 961
+        Integer numberOfColumns = hexagonBuilder.numberOfColumns(hexagonBuilder.xOffset())
         then:
         assert numberOfColumns == 6
     }
 
     void "calculate number of columns one pixel too few"() {
         when:
-        hexelBuilder.radius = 101
-        hexelBuilder.width = 960
-        Integer numberOfColumns = hexelBuilder.calculateNoOfColumns(hexelBuilder.calculateXOffset())
+        hexagonBuilder.radius = 101
+        hexagonBuilder.width = 960
+        Integer numberOfColumns = hexagonBuilder.numberOfColumns(hexagonBuilder.xOffset())
         then:
         assert numberOfColumns == 5
     }
 
     void "calculate number of rows fits exactly"() {
         when:
-        hexelBuilder.height = 954
-        Integer numberOfRows = hexelBuilder.calculateNoOfRows(hexelBuilder.calculateYOffset())
+        hexagonBuilder.height = 954
+        Integer numberOfRows = hexagonBuilder.numberOfRows(hexagonBuilder.yOffset())
         then:
         assert numberOfRows == 5
     }
 
     void "calculate number of rows fone pixel too few"() {
         when:
-        hexelBuilder.height = 953
-        Integer numberOfRows = hexelBuilder.calculateNoOfRows(hexelBuilder.calculateYOffset())
+        hexagonBuilder.height = 953
+        Integer numberOfRows = hexagonBuilder.numberOfRows(hexagonBuilder.yOffset())
         then:
         assert numberOfRows == 4
     }
