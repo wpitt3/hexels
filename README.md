ef8699cc34d67cc61a7a37953348e75822bc9c3f
Will Pitt <wpitt3@gmail.com>
2018-08-10 23:23:11 +0100

    size can be set, but is dodgey

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ install gradle 4.1
 run `gradle build`
 
 ## To run
-java -jar build/libs/hexels-1.0-SNAPSHOT.jar *imageInPath* *imageOutPath*
+java -jar build/libs/hexels-1.0-SNAPSHOT.jar *imageInPath* *imageOutPath* *size(optional [0-9])*
 
 
--- src/main/groovy/hexels/Main.groovy
+++ src/main/groovy/hexels/Main.groovy
@@ -8,9 +8,25 @@ if (args.length < 2) {
 println "From file ${args[0]}"
 println "To file ${args[1]}"
 
+int size = parseSize(args)
+println "Size $size"
+
 List obama = IRW.convertImageToArray(IRW.readBufferedImage(args[0]))
-HexelBuilder hexelBuilder = new HexelBuilder(obama.size(), obama[0].size())
+
+HexelBuilder hexelBuilder = new HexelBuilder(obama.size(), obama[0].size(), size)
 
 List hexagons = hexelBuilder.createHexagonsIndexes()
 List image = hexelBuilder.createHexImage(hexagons, obama)
 IRW.writeBufferedImage(IRW.convertArrayToImage(image), args[1])
+
+int parseSize(String[] args) {
+    List sizes = [10, 14, 22, 26, 34, 38, 46, 50, 54, 58]
+    if (args.length > 2) {
+        try {
+            int size = Integer.parseInt(args[2])
+            return size < 10 ? sizes[size] : sizes[2]
+        } catch (Exception e) {
+        }
+    }
+    return sizes[2]
+}
